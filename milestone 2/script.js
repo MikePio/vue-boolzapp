1af223fc5ebc22ae10494065327a525083692f64
MikePio <michelepiopilla@gmail.com>
2023-04-19 17:44:29 +0200

    Fixed active class to the click

--- milestone 2/script.js
+++ milestone 2/script.js
@@ -4,59 +4,206 @@ createApp({
   data(){
     return{
     title: 'To Do List',
-        chats: [
-          {
-            name: 'Michele test',
-            img: 'avatar_1',
-            activeChat: true
-          },
-          {
-            name: 'Fabio',
-            img: 'avatar_2',
-            activeChat: false
-          },
-          {
-            name: 'Luca',
-            img: 'avatar_3',
-            activeChat: false 
-          },
-          {
-            name: 'Franco',
-            img: 'avatar_4',
-            activeChat: false
-          },
-          {
-            name: 'Dario',
-            img: 'avatar_5',
-            activeChat: false
-          },
-          {
-            name: 'Maria',
-            img: 'avatar_6',
-            activeChat: false
-          },
-          {
-            name: 'Massimo',
-            img: 'avatar_7',
-            activeChat: false
-          },
-          {
-            name: 'Luigi',
-            img: 'avatar_8',
-            activeChat: false
-          },
-          {
-            name: 'Marco',
-            img: 'avatar_2',
-            activeChat: false
-          },
-        ]
+    chatActive: 0,
+    chats: [
+      {
+          name: 'Michele',
+          img: '1',
+          activeChat: true,
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Hai portato a spasso il cane?',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Ricordati di stendere i panni',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 16:15:22',
+                  message: 'Tutto fatto!',
+                  status: 'received'
+              }
+          ],
+      },
+      {
+          name: 'Fabio',
+          img: '2',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '20/03/2020 16:30:00',
+                  message: 'Ciao come stai?',
+                  status: 'sent'
+              },
+              {
+                  date: '20/03/2020 16:30:55',
+                  message: 'Bene grazie! Stasera ci vediamo?',
+                  status: 'received'
+              },
+              {
+                  date: '20/03/2020 16:35:00',
+                  message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                  status: 'sent'
+              }
+          ],
+      },
+      {
+          name: 'Samuele',
+          img: '3',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '28/03/2020 10:10:40',
+                  message: 'La Marianna va in campagna',
+                  status: 'received'
+              },
+              {
+                  date: '28/03/2020 10:20:10',
+                  message: 'Sicuro di non aver sbagliato chat?',
+                  status: 'sent'
+              },
+              {
+                  date: '28/03/2020 16:15:22',
+                  message: 'Ah scusa!',
+                  status: 'received'
+              }
+          ],
+      },
+      {
+          name: 'Alessandro B.',
+          img: '4',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Lo sai che ha aperto una nuova pizzeria?',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Si, ma preferirei andare al cinema',
+                  status: 'received'
+              }
+          ],
+      },
+      {
+          name: 'Alessandro L.',
+          img: '5',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Ricordati di chiamare la nonna',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Va bene, stasera la sento',
+                  status: 'received'
+              }
+          ],
+      },
+      {
+          name: 'Claudia',
+          img: '6',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Ciao Claudia, hai novità?',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Non ancora',
+                  status: 'received'
+              },
+              {
+                  date: '10/01/2020 15:51:00',
+                  message: 'Nessuna nuova, buona nuova',
+                  status: 'sent'
+              }
+          ],
+      },
+      {
+          name: 'Federico',
+          img: '7',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                  status: 'received'
+              }
+          ],
+      },
+      {
+          name: 'Luca',
+          img: '2',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                  status: 'received'
+              }
+          ],
+      },
+      {
+          name: 'Davide',
+          img: '8',
+          activeChat: false,
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                  status: 'received'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:51:00',
+                  message: 'OK!!',
+                  status: 'received'
+                }
+              ],
+            }
+          ],
+
+          
+          
       }
-    },
+    },  
     
     methods:{
-
-
+      classActive(index){
+        this.chatActive = index;
+      }
     }
 
   }).mount('#app');
