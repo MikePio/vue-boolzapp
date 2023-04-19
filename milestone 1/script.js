762202558f4775fa0e38024ca4a2ae10cfe3270e
MikePio <michelepiopilla@gmail.com>
2023-04-19 12:13:16 +0200

    Completed milestone 1

--- milestone 1/script.js
+++ milestone 1/script.js
@@ -1,4 +1,55 @@
+const {createApp} = Vue;
 
+createApp({
+  data(){
+    return{
+    title: 'To Do List',
+        chats: [
+          {
+            name: 'Michele',
+            img: 'avatar_1'
+          },
+          {
+            name: 'Fabio',
+            img: 'avatar_2' 
+          },
+          {
+            name: 'Luca',
+            img: 'avatar_3' 
+          },
+          {
+            name: 'Franco',
+            img: 'avatar_4' 
+          },
+          {
+            name: 'Dario',
+            img: 'avatar_5' 
+          },
+          {
+            name: 'Maria',
+            img: 'avatar_6' 
+          },
+          {
+            name: 'Massimo',
+            img: 'avatar_7' 
+          },
+          {
+            name: 'Luigi',
+            img: 'avatar_8' 
+          },
+          {
+            name: 'Marco',
+            img: 'avatar_2' 
+          },
+        ]
+      }
+    },
+    
+    methods:{
+
+    }
+
+  }).mount('#app');
 
 
 
